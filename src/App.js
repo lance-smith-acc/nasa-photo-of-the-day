afeb7c7e20ddbbbd3006a3cc1db913c7d3bc630f
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2019-12-12 15:41:02 -0600

    Added styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { useState, useEffect } from "react";
+import { Container, Row, Col, Card, CardTitle, CardHeader, CardImg, CardText, CardBody,
+  CardSubtitle } from 'reactstrap';
 import "./App.css";
 
 function App() {
@@ -8,25 +11,34 @@ function App() {
   
   useEffect(() => {
      axios
-    .get('https://api.nasa.gov/planetary/apod?api_key=PXJFppzvJQCIAb14zE2m5ePqKsN9DVaofbuvi3fJ')
+    .get('https://api.nasa.gov/planetary/apod?api_key=JGeDYqqdJG7TIC5esXKLRw1qe4eVFcQsuxEeNcwU')
     .then(response => {
       console.log(response.data);
       setObj(response.data);
     })
     .catch(error => console.log(error))
-  }, [imageObj])
+  }, [])
  
   return (
-    <div className="App">
-      <div className="container">
-        <img src={imageObj.url} alt={imageObj.title}></img>
-        <div className="textContent">
-          <h1 className="title">{imageObj.title}</h1>
-          <h2>{imageObj.date}</h2>
-          <p>{imageObj.explanation}</p>
-        </div>
-      </div>      
-    </div>
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs={1}></Col>
+        <Col xs={10}><Card body className="text-center">
+          <CardImg top src={imageObj.url} alt={imageObj.title} />
+            <CardBody style={{backgroundColor:'#D4D1D0'}}>
+              <CardHeader>
+                <CardTitle tag="h1">{imageObj.title}</CardTitle>
+                <CardSubtitle tag="h4">{imageObj.date}</CardSubtitle>
+              </CardHeader>
+              <CardText className="mt-2">{imageObj.explanation}</CardText>
+            </CardBody>
+        </Card></Col>
+        <Col xs={1}></Col>
+        
+        
+      </Row>
+         
+    </Container>
   );
 }
 

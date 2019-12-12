df0b81c9b0772cadd53d2b631d945cc863d0224e
Lance Smith <57012297+lance-smith-acc@users.noreply.github.com>
2019-12-11 19:04:52 -0600

    Completed card

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [imageObj, setObj] = useState([]);
+  
+  useEffect(() => {
+     axios
+    .get('https://api.nasa.gov/planetary/apod?api_key=PXJFppzvJQCIAb14zE2m5ePqKsN9DVaofbuvi3fJ')
+    .then(response => {
+      console.log(response.data);
+      setObj(response.data);
+    })
+    .catch(error => console.log(error))
+  }, [imageObj])
+ 
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
-      </p>
+      <div className="container">
+        <img src={imageObj.url} alt={imageObj.title}></img>
+        <div className="textContent">
+          <h1 className="title">{imageObj.title}</h1>
+          <h2>{imageObj.date}</h2>
+          <p>{imageObj.explanation}</p>
+        </div>
+      </div>      
     </div>
   );
 }
